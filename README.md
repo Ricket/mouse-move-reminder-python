036e03d23da83e9e691a7fa6d441f0679d74b48a
Richard Carter <richard@richardcarter.org>
2022-02-17 14:31:58 -0600

    play sound

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-pip install mouse pywin32 psutil
+Download ffmpeg: https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-full.7z
+
+Extract it into this folder. Update the variable `ffmpeg_bin` at top of main.py to point to the bin subfolder.
+
+Install Python dependencies:
+
+(TODO confirm if simpleaudio is needed)
+
+```
+pip install mouse pywin32 psutil simpleaudio pydub
+```
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import mouse
 import time
 import win32gui, win32process, psutil
+from pathlib import Path
+import os
+
+curr_dir = Path(__file__).parent
+
+# Put ffmpeg into path so that pydub picks it up
+ffmpeg_bin = curr_dir.joinpath('ffmpeg-2022-02-17-git-2812508086-full_build/bin')
+os.environ['PATH'] = str(ffmpeg_bin) + ';' + os.environ['PATH']
+
+from pydub import AudioSegment
+from pydub.playback import play
+
+idle_warning_sound_path = curr_dir.joinpath('sfx/NaviHeyListen.mp3')
+idle_warning_sound = AudioSegment.from_mp3(str(idle_warning_sound_path))
+
+def do_idle_warning():
+    print('warning, idle for too long!')
+    play(idle_warning_sound)
+    # stop timer
 
 def active_window_process_name():
     try:
@@ -20,6 +39,8 @@ def mouseHandler(event):
         print('TODO reset timer')
         
 
-mouse.hook(mouseHandler)
+#mouse.hook(mouseHandler)
 
-time.sleep(120)
+#time.sleep(120)
+
+do_idle_warning()
